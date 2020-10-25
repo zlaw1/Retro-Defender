effa7ac2128cdb1d86d65876532b72b8d0bf57cd
Zachary Law <zachlaw90@gmail.com>
2020-10-24 23:13:45 -0400

    first commit of project
    player movemnt 2d and enemy following and return to home

---
+++ Assets/CharMove.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMove : MonoBehaviour
+{
+    Animator anim; 
+    [SerializeField] float speed = 2.0f;
+    Vector2 motionVector; 
+    public Vector2 lastMotionVector;
+    private Rigidbody2D rb;
+    public bool moving;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.velocity = motionVector * speed;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        motionVector = new Vector2(
+            horizontal,
+            vertical
+            );
+
+        anim.SetFloat("Horizontal", horizontal);
+        anim.SetFloat("Vertical", vertical);
+
+        moving = horizontal != 0 || vertical != 0;
+        anim.SetBool("Moving", moving);
+
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            lastMotionVector = new Vector2(
+            horizontal,
+            vertical
+            ).normalized;
+
+            anim.SetFloat("lastHorizontal", horizontal);
+            anim.SetFloat("lastVertical", vertical);
+
+
+        }
+
+    }
+    
+}
---
+++ Assets/EnemyMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    Animator anim;
+    [SerializeField] private float speed = 5f;
+    Rigidbody2D rb;
+    private Transform player;
+    [SerializeField] float range = 2f;
+    [SerializeField] float minRange = 1f;
+    public Transform homeSpot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        player = FindObjectOfType<CharMove>().transform;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(player.position, transform.position) <= range && Vector3.Distance(player.position, transform.position) > minRange)
+        {
+        FollowPlayer();
+        }
+        else if(Vector3.Distance(player.position, - transform.position) >= range)
+        {
+            GoToHome();
+        }
+
+    }
+    public void FollowPlayer()
+    {
+        anim.SetBool("Moving", true);
+        anim.SetFloat("DirX", player.transform.position.x - transform.position.x);
+        anim.SetFloat("DirY", player.transform.position.y - transform.position.y);
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed *Time.deltaTime);
+    }
+    public void GoToHome()
+    {
+        anim.SetBool("Moving", true);
+        anim.SetFloat("DirX", homeSpot.transform.position.x - transform.position.x);
+        anim.SetFloat("DirY", homeSpot.transform.position.y - transform.position.y);
+        transform.position = Vector3.MoveTowards(transform.position, homeSpot.position, speed * Time.deltaTime);
+    }
+}
