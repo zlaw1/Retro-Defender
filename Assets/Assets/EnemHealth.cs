999c0ebf40428f476154d02cffd8e7c4c2a34e9a
Zachary Law <zachlaw90@gmail.com>
2020-10-25 01:16:59 -0400

    attack working
    attack now kills enemy

---
+++ Assets/Assets/EnemHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    int currentHealth;
+    public 
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if(currentHealth <= 0)Destroy(gameObject);
+    }
+}
